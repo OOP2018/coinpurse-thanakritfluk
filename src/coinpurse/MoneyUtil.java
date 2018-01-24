6b2a63333692627788f7fe97bbd36d35eb20867f
thanakritfluk <thanakrit.da@ku.th>
2018-01-24 20:37:40 +0700

    Add filterByCurrency method and add test of this method.

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -9,11 +9,17 @@ public class MoneyUtil {
         List<Coin> coins = new ArrayList<Coin>();
         coins.add(new Coin(10.0, "Baht"));
         coins.add(new Coin(0.5, "Baht"));
+        coins.add(new Coin(25.0, "Dollar"));
         coins.add(new Coin(2.0, "Baht"));
+        coins.add(new Coin(2.0, "Dollar"));
         coins.add(new Coin(1.0, "Baht"));
         printCoins(coins);
         sortCoins(coins);
         printCoins(coins);
+
+        List<Coin> testFilter = filterByCurrency(coins,"Dollar");
+        System.out.println("Sort Currency: "+testFilter);
+
     }
 
     /**
@@ -30,8 +36,26 @@ public class MoneyUtil {
      *
      * @param coins is the list include value and currency
      */
-    public static void printCoins(List coins) {
+    public static void printCoins(List<Coin> coins) {
         System.out.println(coins);
+    }
+
+
+    /**
+     * This method use to sort the coin with same currency and return by list.
+     * @param coins is the list contain value and currency of coins.
+     * @param currency ts the currency of types that use to check.
+     * @return the list of the coin that sort by currency
+     */
+    public static List<Coin> filterByCurrency(List<Coin> coins,String currency){
+        List<Coin> sortCurrency = new ArrayList<Coin>();
+        for(Coin a : coins) {
+            if(a.getCurrency().toLowerCase().equals(currency.toLowerCase())) {
+                sortCurrency.add(a);
+            }
+        }
+        return sortCurrency;
+
     }
 
 
