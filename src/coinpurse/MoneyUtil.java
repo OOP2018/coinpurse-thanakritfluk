c83bc4e8e2933e870ff19829ad22213b506171fe
Thanakritfluk <thanakrit.da@ku.th>
2018-04-20 14:34:22 +0700

    Finish Lab13.

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 /**
  * this is testing coin class.
+ *
  * @author Thanakrit Daorueang
  */
 public class MoneyUtil {
@@ -19,11 +20,11 @@ public class MoneyUtil {
         valuables.add(new Coin(2.0, "Dollar"));
         valuables.add(new Coin(1.0, "Baht"));
         printCoins(valuables);
-        sortCoins(valuables);
+        sortMoney(valuables);
         printCoins(valuables);
 
-        List<Valuable> testFilter = filterByCurrency(valuables,"Dollar");
-        System.out.println("Sort Currency: "+testFilter);
+        List<Valuable> testFilter = filterByCurrency(valuables, "Dollar");
+        System.out.println("Sort Currency: " + testFilter);
 
     }
 
@@ -32,7 +33,7 @@ public class MoneyUtil {
      *
      * @param valuables is the list include value and currency
      */
-    public static void sortCoins(List<Valuable> valuables) {
+    public static void sortMoney(List<? extends Valuable> valuables) {
         Comparator<Valuable> comp = new ValueComparator();
         valuables.sort(comp);
     }
@@ -43,20 +44,21 @@ public class MoneyUtil {
      * @param valuables is the list include value and currency
      */
     public static void printCoins(List<Valuable> valuables) {
-        System.out.println("All we have: "+valuables);
+        System.out.println("All we have: " + valuables);
     }
 
 
     /**
      * This method use to sort the coin with same currency and return by list.
+     *
      * @param valuables is the list contain value and currency of valuables.
-     * @param currency ts the currency of types that use to check.
+     * @param currency  ts the currency of types that use to check.
      * @return the list of the coin that sort by currency
      */
-    public static List<Valuable> filterByCurrency(List<Valuable> valuables,String currency){
-        List<Valuable> sortCurrency = new ArrayList<Valuable>();
-        for(Valuable a : valuables) {
-            if(a.getCurrency().toLowerCase().equals(currency.toLowerCase())) {
+    public static <E extends Valuable> List<E> filterByCurrency(List<E> valuables, String currency) {
+        List<E> sortCurrency = new ArrayList<>();
+        for (E a : valuables) {
+            if (a.getCurrency().toLowerCase().equals(currency.toLowerCase())) {
                 sortCurrency.add(a);
             }
         }
@@ -64,5 +66,17 @@ public class MoneyUtil {
 
     }
 
-
+    /**
+     * Return the larger argument, based on sort order, using
+     * the objects' own compareTo method for comparing.
+     *
+     * @param args one or more Comparable objects to compare.
+     * @return the argument that would be last if sorted the elements.
+     * @throws IllegalArgumentException if no arguments given.
+     */
+    public static <E extends Comparable<? super E>> E max(E... args) {
+        E max = args[0];
+        for (E a : args) if (max.compareTo(a) < 0) max = a;
+        return max;
+    }
 }
